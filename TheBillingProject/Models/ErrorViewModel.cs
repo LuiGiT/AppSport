fd19fc65c34be29a0855e1de74f7f743f6d38e3e
Jose Abimael Martinez <jamartinez@atlantica.do>
2018-11-01 11:30:09 -0400

    first commit

---
+++ TheBillingProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheBillingProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
